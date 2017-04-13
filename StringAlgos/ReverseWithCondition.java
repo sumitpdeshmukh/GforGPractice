b7a99720a83b56fcd86a49f027ba721d19f4a84f
Sumit Deshmukh <sumo1700@gmail.com>
2017-04-13 01:00:11 -0400

    Update ReverseWithCondition.java

--- StringAlgos/ReverseWithCondition.java
+++ StringAlgos/ReverseWithCondition.java
@@ -24,3 +24,43 @@ class ReverseArray
 	    System.out.println(String.valueOf(arr));
 	}
 }
+
+// Given a string, print all possible palindromic partitions
+class AllPalindrome
+{
+	// Driver method
+	public static void main(String[] args)
+	{
+	    String str = "geeks";
+	    char[] arr = str.toCharArray();
+	    int len = str.length();
+	    int pass = 0;
+	    while (pass < len) {
+    	    for (int i = 0; i <= len - pass; i++) {
+    	            String mystr = str.substring(i, pass + i);
+    	            if(isPalindrome(mystr)) {
+    	                System.out.print(mystr + " ");
+    	            }
+    	    }
+    	    System.out.println();
+    	    pass++;
+    	}
+	    //System.out.println(String.valueOf(arr));
+	}
+	
+	public static boolean isPalindrome(String str) {
+	    char[] arr = str.toCharArray();
+	    int i=0;
+	    int j= arr.length -1;
+
+        while (i < j && arr[i] == arr[j]) {
+            i++;
+            j--;
+        }
+        
+        if (j <= i)
+            return true;
+        else
+            return false;
+	}
+}
